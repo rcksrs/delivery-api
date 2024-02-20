1005bd954a4ab3956c5ae17e3f83ba57b5c2dc20
Eryck Soares <rcksrs@outlook.com>
2024-02-20 18:57:09 -0300

    feat: including global exceptions

---
+++ src/main/java/com/rcksrs/delivery/core/exception/global/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+package com.rcksrs.delivery.core.exception.global;
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
