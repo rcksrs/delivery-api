ce5896564b0b088d7912bd0e17669a82f997ee70
Eryck Soares <rcksrs@outlook.com>
2024-02-21 15:32:10 -0300

    feat: mapping user entity

--- src/main/java/com/rcksrs/delivery/core/domain/entity/BaseEntity.java
+++ src/main/java/com/rcksrs/delivery/core/domain/entity/BaseEntity.java
@@ -3,11 +3,18 @@ package com.rcksrs.delivery.core.domain.entity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@MappedSuperclass
 public abstract class BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime createdAt = LocalDateTime.now();
     private LocalDateTime modifiedAt;
--- src/main/java/com/rcksrs/delivery/core/domain/entity/User.java
+++ src/main/java/com/rcksrs/delivery/core/domain/entity/User.java
@@ -3,14 +3,23 @@ package com.rcksrs.delivery.core.domain.entity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 @Data
+@Entity
 @EqualsAndHashCode(callSuper = true)
 public class User extends BaseEntity {
     private String name;
     private String email;
     private String password;
-    private String document;
     private String phone;
+
+    @Embedded
     private Address address;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 }
