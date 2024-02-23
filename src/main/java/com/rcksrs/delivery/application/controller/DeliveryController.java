4c4e641ba2fb210eda5870c8b626742c65e54798
Eryck Soares <rcksrs@outlook.com>
2024-02-22 22:40:36 -0300

    feat: updating delivery response and including controller

---
+++ src/main/java/com/rcksrs/delivery/application/controller/DeliveryController.java
@@ -0,0 +1,111 @@
+package com.rcksrs.delivery.application.controller;
+
+import com.rcksrs.delivery.core.domain.dto.delivery.DeliveryFilter;
+import com.rcksrs.delivery.core.domain.dto.delivery.DeliveryResponse;
+import com.rcksrs.delivery.core.domain.dto.delivery.SaveDeliveryRequest;
+import com.rcksrs.delivery.core.domain.dto.delivery.UpdateDeliveryRequest;
+import com.rcksrs.delivery.core.domain.entity.DeliveryStatus;
+import com.rcksrs.delivery.core.exception.global.ExceptionMessage;
+import com.rcksrs.delivery.core.usecase.delivery.DeleteDeliveryUseCase;
+import com.rcksrs.delivery.core.usecase.delivery.FindDeliveryUseCase;
+import com.rcksrs.delivery.core.usecase.delivery.SaveDeliveryUseCase;
+import com.rcksrs.delivery.core.usecase.delivery.UpdateDeliveryUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/v1/delivery")
+@RequiredArgsConstructor
+@Tag(name = "Delivery Controller")
+public class DeliveryController {
+    private final FindDeliveryUseCase findDeliveryUseCase;
+    private final SaveDeliveryUseCase saveDeliveryUseCase;
+    private final UpdateDeliveryUseCase updateDeliveryUseCase;
+    private final DeleteDeliveryUseCase deleteDeliveryUseCase;
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Buscar entrega a partir do id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ExceptionMessage.class)))
+    })
+    public ResponseEntity<DeliveryResponse> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(findDeliveryUseCase.findById(id));
+    }
+
+    @GetMapping
+    @Operation(summary = "Listar entregas a partir do filtro de busca")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200")
+    })
+    @Parameters({
+            @Parameter(name = "pageNumber", description = "page number", in = ParameterIn.QUERY,
+                    schema = @Schema(type = "integer",  defaultValue = "0")),
+            @Parameter(name = "pageSize", description = "page size", in = ParameterIn.QUERY,
+                    schema = @Schema(type = "integer",  defaultValue = "5")),
+            @Parameter(name = "name", description = "sort specification", in = ParameterIn.QUERY,
+                    schema = @Schema(type = "string"))
+    })
+    public ResponseEntity<Page<DeliveryResponse>> search(DeliveryFilter filter,
+                                                         @Parameter(hidden = true) @PageableDefault() Pageable pageable) {
+        return ResponseEntity.ok(findDeliveryUseCase.findByFilter(filter, pageable));
+    }
+
+    @PostMapping
+    @Operation(summary = "Salvar entrega")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ExceptionMessage.class))),
+    })
+    public ResponseEntity<DeliveryResponse> save(@RequestBody SaveDeliveryRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveDeliveryUseCase.save(request));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Atualizar informações da entrega")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ExceptionMessage.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ExceptionMessage.class)))
+    })
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody UpdateDeliveryRequest request) {
+        updateDeliveryUseCase.update(id, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/status")
+    @Operation(summary = "Atualizar status da entrega")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ExceptionMessage.class)))
+    })
+    public ResponseEntity<Void> updateStatus(@PathVariable Long id, @RequestBody DeliveryStatus status) {
+        updateDeliveryUseCase.updateStatus(id, status);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir entrega")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ExceptionMessage.class)))
+    })
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        deleteDeliveryUseCase.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/com/rcksrs/delivery/core/domain/dto/delivery/DeliveryResponse.java
+++ src/main/java/com/rcksrs/delivery/core/domain/dto/delivery/DeliveryResponse.java
@@ -5,8 +5,13 @@ import com.rcksrs.delivery.core.domain.entity.Address;
 import com.rcksrs.delivery.core.domain.entity.Delivery;
 import com.rcksrs.delivery.core.domain.entity.DeliveryStatus;
 
-public record DeliveryResponse(String info, DeliveryStatus status, Address location, OrderResponse order) {
+public record DeliveryResponse(Long id, String info, DeliveryStatus status, Address location, OrderResponse order) {
     public DeliveryResponse(Delivery delivery) {
-        this(delivery.getInfo(), delivery.getStatus(), delivery.getLocation(), new OrderResponse(delivery.getOrder()));
+        this(delivery.getId(),
+                delivery.getInfo(),
+                delivery.getStatus(),
+                delivery.getLocation(),
+                new OrderResponse(delivery.getOrder())
+        );
     }
 }
