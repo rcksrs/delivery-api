144bf9f594ffc5a486c90a9d4979b7eaf86f31f8
Eryck Soares <rcksrs@outlook.com>
2024-02-24 20:29:04 -0300

    fix: insert user with wrong role

--- src/main/resources/db/migration/V5__insert_admin_user.sql
+++ src/main/resources/db/migration/V5__insert_admin_user.sql
@@ -1,4 +1,4 @@
 INSERT INTO "user"
-(id, active, created_at, modified_at, city, country, "number", state, street, zip_code, email, "name", "password", phone, "role")
+(active, created_at, modified_at, city, country, "number", state, street, zip_code, email, "name", "password", phone, "role")
 VALUES
-(1, true, '2024-02-24 18:27:17.107', NULL, 'João Pessoa', 'Brasil', '100', 'Paraíba', 'Rua 10, Centro', '58035000', 'admin@email.com', 'Admin', '$2a$10$ekYnTUZQfgDfyMKzsxCfSu8JNe1vxfS44bIw2sq1vCIvu.rPyBheS', '900001111', 'USER');
+(true, '2024-02-24 18:27:17.107', NULL, 'João Pessoa', 'Brasil', '100', 'Paraíba', 'Rua 10, Centro', '58035000', 'admin@email.com', 'Admin', '$2a$10$ekYnTUZQfgDfyMKzsxCfSu8JNe1vxfS44bIw2sq1vCIvu.rPyBheS', '900001111', 'ADMIN');
