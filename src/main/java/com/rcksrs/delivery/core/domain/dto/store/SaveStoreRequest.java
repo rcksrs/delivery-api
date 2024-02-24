a4a2402cf37e91bf927ca0d765cabbe1fa94ff5f
Eryck Soares <rcksrs@outlook.com>
2024-02-24 20:30:08 -0300

    fix: email validation missing for SaveStoreRequest

--- src/main/java/com/rcksrs/delivery/core/domain/dto/store/SaveStoreRequest.java
+++ src/main/java/com/rcksrs/delivery/core/domain/dto/store/SaveStoreRequest.java
@@ -4,16 +4,18 @@ import com.rcksrs.delivery.core.domain.entity.Address;
 import com.rcksrs.delivery.core.domain.entity.Store;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public record SaveStoreRequest(
         @NotBlank String name,
-        String email,
+        @Email String email,
         @NotBlank @Size(min = 9, max = 13) String phone,
         String description,
         @NotNull @Valid Address address) {
+
     public Store toEntity() {
         var store = new Store();
         store.setName(this.name);
