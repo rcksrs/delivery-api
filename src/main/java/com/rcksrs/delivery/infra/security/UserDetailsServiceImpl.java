ce6ca494153f3a1e69440a9c182a6ee52f640b09
Eryck Soares <rcksrs@outlook.com>
2024-02-25 13:02:40 -0300

    fix: changing exception on UserDetailsServiceImpl

--- src/main/java/com/rcksrs/delivery/infra/security/UserDetailsServiceImpl.java
+++ src/main/java/com/rcksrs/delivery/infra/security/UserDetailsServiceImpl.java
@@ -1,6 +1,6 @@
 package com.rcksrs.delivery.infra.security;
 
-import com.rcksrs.delivery.core.exception.global.UnauthorizedException;
+import com.rcksrs.delivery.core.exception.user.UserNotFoundException;
 import com.rcksrs.delivery.core.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,8 +13,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UnauthorizedException {
-        var user = userRepository.findByEmailAndActiveTrue(username).orElseThrow(UnauthorizedException::new);
+    public UserDetails loadUserByUsername(String username) throws UserNotFoundException {
+        var user = userRepository.findByEmailAndActiveTrue(username).orElseThrow(UserNotFoundException::new);
         return new UserDetailsEntity(user);
     }
 }
